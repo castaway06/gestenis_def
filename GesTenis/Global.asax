d3c2d955cd0f0e906ea6adafb4a7d60d6d6badf3
castaway06 <castaway06@gmail.com>
2015-08-01 16:17:13 +0200

    Initial commit

---
+++ GesTenis/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GesTenis.MvcApplication" Language="C#" %>
---
+++ GesTenis/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GesTenis.Startup))]
+namespace GesTenis
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
