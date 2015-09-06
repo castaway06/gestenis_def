532596114c7bffa9302bc7e311b9b46a78998270
castaway06 <castaway06@gmail.com>
2015-09-06 11:16:07 +0200

    + generacion xml factura

--- GesTenis/tools/Tools.cs
+++ GesTenis/tools/Tools.cs
@@ -77,10 +77,54 @@ namespace GesTenis.tools
 
         public static string generarXmlFactura(facturas factura, reservas reserva)
         {
+
             string ret = "xml para factura " + factura.id + ". Socio: " + reserva.socios.nombre + " " + reserva.socios.apellidos
                 + ". Ha reservado el recurso " + reserva.recursos.nombre_rec + " el dia " + reserva.fecha + " a las " + reserva.hora
                 + ". El precio es de " + reserva.precio + " Euros.";
-            return ret;
+
+            string ret2 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                        + "<xfdf xmlns=\"http://ns.adobe.com/xfdf/\" xml:space=\"preserve\">"
+                        + "<f href=\"http://localhost:57570/App_Data\" />"
+                        + "<fields>"
+                            + "<field name = \"n_factura\">"
+                            + "<value>" + factura.id.ToString() + "</value>"
+                            + "</field>"
+                            + "<field name = \"f_factura\">"
+                            + "<value>" + DateTime.Today.ToString("dd/MM/yyyy") + "</value>"
+                            + "</field>"
+                            + "<field name = \"id_socio\">"
+                            + "<value>" + reserva.socios.id + "</value>"
+                            + "</field>"
+                            + "<field name = \"nombre\">"
+                            + "<value>" + reserva.socios.nombre + reserva.socios.apellidos + "</value>"
+                            + "</field>"
+                            + "<field name = \"nif\">"
+                            + "<value>" + reserva.socios.nif + "</value>"
+                            + "</field>"
+                            + "<field name = \"direccion1\">"
+                            + "<value>" + reserva.socios.direccion1 + "</value>"
+                            + "</field>"
+                            + "<field name = \"direccion2\">"
+                            + "<value>" + reserva.socios.direccion2 + "</value>"
+                            + "</field>"
+                            + "<field name = \"nombre_recurso\">"
+                            + "<value>" + reserva.recursos.nombre_rec + "</value>"
+                            + "</field>"
+                            + "<field name = \"fecha\">"
+                            + "<value>" + reserva.fecha.Date.ToString() + "</value>"
+                            + "</field>"
+                            + "<field name = \"hora\">"
+                            + "<value>" + reserva.hora.Hour.ToString() + "</value>"
+                            + "</field>"
+                            + "<field name = \"precio\">"
+                            + "<value>" + reserva.precio.ToString() + "</value>"
+                            + "</field>"
+                            + "<field name = \"precio_total\">"
+                            + "<value>" + reserva.precio.ToString() + "</value>"
+                            + "</field>"
+                        + "</fields>";
+
+            return ret2;
         }
 
     }
